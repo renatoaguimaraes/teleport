f45b2842a2c589f59eca2b075565f16b83f7f8ac
Nic Klaassen <nic@goteleport.com>
2021-04-29 21:07:29 -0700

    don't switch kube context by default

--- lib/kube/kubeconfig/kubeconfig.go
+++ lib/kube/kubeconfig/kubeconfig.go
@@ -111,10 +111,14 @@ func UpdateWithClient(ctx context.Context, path string, tc *client.TeleportClien
 		if err != nil && !trace.IsNotFound(err) {
 			return trace.Wrap(err)
 		}
-		// Use the same defaulting as the auth server.
-		v.Exec.SelectCluster, err = kubeutils.CheckOrSetKubeCluster(ctx, ac, tc.KubernetesCluster, v.TeleportClusterName)
-		if err != nil && !trace.IsNotFound(err) {
-			return trace.Wrap(err)
+
+		// Only switch the current context if kube-cluster is explicitly set on the command line.
+		if tc.KubernetesCluster != "" {
+			// Use the same defaulting as the auth server.
+			v.Exec.SelectCluster, err = kubeutils.CheckOrSetKubeCluster(ctx, ac, tc.KubernetesCluster, v.TeleportClusterName)
+			if err != nil && !trace.IsNotFound(err) {
+				return trace.Wrap(err)
+			}
 		}
 
 		// If there are no registered k8s clusters, we may have an older
